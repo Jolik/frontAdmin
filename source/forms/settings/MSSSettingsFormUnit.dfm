f7b42bf798886e126e0e2d98cf854d858c50ca27
Dzhalil Akhtyamov <ajolik@gmail.com>
2025-11-21 00:19:12 +0300

    Add MSS settings form

---
+++ source/forms/settings/MSSSettingsFormUnit.dfm
@@ -0,0 +1,49 @@
+object MSSSettingsForm: TMSSSettingsForm
+  Left = 0
+  Top = 0
+  ClientHeight = 600
+  ClientWidth = 800
+  Caption = 'MSSSettingsForm'
+  OnCreate = UniFormCreate
+  OldCreateOrder = False
+  MonitoredKeys.Keys = <>
+  PixelsPerInch = 96
+  TextHeight = 15
+  object tvNavigate: TUniTreeView
+    Left = 0
+    Top = 0
+    Width = 200
+    Height = 600
+    Hint = ''
+    Align = alLeft
+    Items.NodeData = {0100000000000000}
+    TabOrder = 0
+    OnChange = tvNavigateChange
+  end
+  object pcForms: TUniPageControl
+    Left = 200
+    Top = 0
+    Width = 600
+    Height = 600
+    Hint = ''
+    ActivePage = tshBlank
+    Align = alClient
+    TabOrder = 1
+    object tshBlank: TUniTabSheet
+      Hint = ''
+      Caption = 'Blank'
+    end
+    object tshRules: TUniTabSheet
+      Hint = ''
+      Caption = 'Rules'
+    end
+    object tshAliases: TUniTabSheet
+      Hint = ''
+      Caption = 'Aliases'
+    end
+    object tshAbonents: TUniTabSheet
+      Hint = ''
+      Caption = 'Abonents'
+    end
+  end
+end
---
+++ source/forms/settings/MSSSettingsFormUnit.pas
@@ -0,0 +1,147 @@
+unit MSSSettingsFormUnit;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics,
+  Controls, Forms, uniGUITypes, uniGUIAbstractClasses,
+  uniGUIClasses, uniGUIForm, uniTreeView, uniPageControl,
+  uniGUIBaseClasses, uniPanel,
+  RulesFormUnit, AliasesFormUnit, AbonentsFormUnit;
+
+type
+  TMSSSettingsForm = class(TUniForm)
+    tvNavigate: TUniTreeView;
+    pcForms: TUniPageControl;
+    tshBlank: TUniTabSheet;
+    tshRules: TUniTabSheet;
+    tshAliases: TUniTabSheet;
+    tshAbonents: TUniTabSheet;
+    procedure UniFormCreate(Sender: TObject);
+    procedure tvNavigateChange(Sender: TObject; Node: TUniTreeNode);
+  private
+    FNodeMain: TUniTreeNode;
+    FNodeComm: TUniTreeNode;
+    FNodeRules: TUniTreeNode;
+    FNodeAliases: TUniTreeNode;
+    FNodeAbonents: TUniTreeNode;
+    FRulesForm: TRulesForm;
+    FAliasesForm: TAliasesForm;
+    FAbonentsForm: TAbonentsForm;
+    procedure InitTree;
+    procedure ShowTabForNode(ANode: TUniTreeNode);
+    procedure EnsureRulesForm;
+    procedure EnsureAliasesForm;
+    procedure EnsureAbonentsForm;
+  public
+  end;
+
+function MSSSettingsForm: TMSSSettingsForm;
+
+implementation
+
+{$R *.dfm}
+
+uses
+  MainModule, uniGUIApplication;
+
+function MSSSettingsForm: TMSSSettingsForm;
+begin
+  Result := TMSSSettingsForm(UniMainModule.GetFormInstance(TMSSSettingsForm));
+end;
+
+procedure TMSSSettingsForm.EnsureAbonentsForm;
+begin
+  if not Assigned(FAbonentsForm) then
+  begin
+    FAbonentsForm := AbonentsForm;
+    FAbonentsForm.Parent := tshAbonents;
+    FAbonentsForm.Align := alClient;
+    FAbonentsForm.BorderStyle := bsNone;
+    FAbonentsForm.Show();
+  end;
+end;
+
+procedure TMSSSettingsForm.EnsureAliasesForm;
+begin
+  if not Assigned(FAliasesForm) then
+  begin
+    FAliasesForm := AliasesForm;
+    FAliasesForm.Parent := tshAliases;
+    FAliasesForm.Align := alClient;
+    FAliasesForm.BorderStyle := bsNone;
+    FAliasesForm.Show();
+  end;
+end;
+
+procedure TMSSSettingsForm.EnsureRulesForm;
+begin
+  if not Assigned(FRulesForm) then
+  begin
+    FRulesForm := RulesForm;
+    FRulesForm.Parent := tshRules;
+    FRulesForm.Align := alClient;
+    FRulesForm.BorderStyle := bsNone;
+    FRulesForm.Show();
+  end;
+end;
+
+procedure TMSSSettingsForm.InitTree;
+begin
+  tvNavigate.Items.BeginUpdate;
+  try
+    tvNavigate.Items.Clear;
+    FNodeMain := tvNavigate.Items.Add(nil, 'Главное');
+    FNodeMain.Data := tshBlank;
+
+    FNodeComm := tvNavigate.Items.AddChild(FNodeMain, 'Коммутация');
+    FNodeComm.Data := tshBlank;
+
+    FNodeRules := tvNavigate.Items.AddChild(FNodeComm, 'Правила');
+    FNodeRules.Data := tshRules;
+
+    FNodeAliases := tvNavigate.Items.AddChild(FNodeComm, 'Алиасы');
+    FNodeAliases.Data := tshAliases;
+
+    FNodeAbonents := tvNavigate.Items.AddChild(FNodeComm, 'Абоненты');
+    FNodeAbonents.Data := tshAbonents;
+
+    FNodeMain.Expand(False);
+    FNodeComm.Expand(False);
+  finally
+    tvNavigate.Items.EndUpdate;
+  end;
+end;
+
+procedure TMSSSettingsForm.ShowTabForNode(ANode: TUniTreeNode);
+var
+  LTarget: TUniTabSheet;
+begin
+  if Assigned(ANode) and (ANode.Data <> nil) then
+    LTarget := TUniTabSheet(ANode.Data)
+  else
+    LTarget := tshBlank;
+
+  if LTarget = tshRules then
+    EnsureRulesForm
+  else if LTarget = tshAliases then
+    EnsureAliasesForm
+  else if LTarget = tshAbonents then
+    EnsureAbonentsForm;
+
+  pcForms.ActivePage := LTarget;
+end;
+
+procedure TMSSSettingsForm.tvNavigateChange(Sender: TObject; Node: TUniTreeNode);
+begin
+  ShowTabForNode(Node);
+end;
+
+procedure TMSSSettingsForm.UniFormCreate(Sender: TObject);
+begin
+  InitTree;
+  tvNavigate.Selected := FNodeMain;
+  ShowTabForNode(FNodeMain);
+end;
+
+end.
